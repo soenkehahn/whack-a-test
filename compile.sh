b78ad14fa173c5d142847b3e775ddd58b459f6ae
Jon Distad <jon.distad@gmail.com>
2018-04-20 10:10:59 -0400

    Original tests pass again

--- compile.sh
+++ compile.sh
@@ -2,10 +2,34 @@
 
 set -eux
 
+TMPDIR="$PWD/.tmp"
+function cleanup {
+    rm -rf "$TMPDIR"
+}
+trap cleanup EXIT
+mkdir -p "$TMPDIR"
+
+rm -rf dist
 mkdir -p dist
+
+cp -R adder "$TMPDIR"
+docker run --rm -v"$TMPDIR":/src/main ponylang/ponyc:0.21.3 ./adder/build.sh "$(id -u)"
+cp "$TMPDIR/adder-bin" dist/adder
+
 cat "$0" | tail -n+$[LINENO+3] > ./dist/whack-a-test
 chmod +x ./dist/whack-a-test
 exit 0
 #!/usr/bin/env bash
-echo "whack-a-test -- a game that Jon and Sönke are playing" >&2
-echo "1 + 2 = 3"
+case "$1" in
+    --help)
+        echo "whack-a-test -- a game that Jon and Sönke are playing" >&2
+        exit 0
+        ;;
+    --add)
+        shift
+        exec "$(dirname $0)/adder" "$@"
+        ;;
+    *)
+        break
+        ;;
+esac
